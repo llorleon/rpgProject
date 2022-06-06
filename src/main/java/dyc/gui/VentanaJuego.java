240a40a3edc72e0692641859aced7d6132c4d776
Victor Martin Llorente <92110001+llorleon@users.noreply.github.com>
2022-06-06 15:54:06 +0200

    Creo Pantalla indicando la Clase Escogida
    Aun sin asignar ni mostrar que clase tienes y que elementos recoge de la misma

--- src/main/java/dyc/gui/VentanaJuego.java
+++ src/main/java/dyc/gui/VentanaJuego.java
@@ -8,17 +8,23 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import dyc.clases.Sesion;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class VentanaJuego extends JPanel {
 
 	private static final long serialVersionUID = 4094030091183746399L;
 
 	public VentanaJuego(Ventana v, Sesion sesion) {
-		setBackground(new Color(255, 0,0 ));
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		JLabel label = new JLabel("aqui el texto");
+		setBackground(Color.BLACK);
+		setLayout(new BorderLayout(0, 0));
+		JLabel lblHasElegidoLa = new JLabel("Has elegido la clase:");
+		lblHasElegidoLa.setFont(new Font("Lucida Grande", Font.PLAIN, 26));
+		lblHasElegidoLa.setHorizontalAlignment(SwingConstants.CENTER);
+		lblHasElegidoLa.setForeground(Color.GREEN);
 		
-		add(label);
+		add(lblHasElegidoLa, BorderLayout.NORTH);
 		
 		setVisible(true);
 	}
