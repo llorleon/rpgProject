d8e59c1cf2c0e75e01c0a1b45d34b99895aa8f5f
Victor Martin Llorente <92110001+llorleon@users.noreply.github.com>
2022-06-10 19:22:27 +0200

    GENERADO JAVADOC

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dyc"},{"l":"dyc.clases"},{"l":"dyc.dao"},{"l":"dyc.db"},{"l":"dyc.exception"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dyc"},{"l":"dyc.clases"},{"l":"dyc.dao"},{"l":"dyc.db"},{"l":"dyc.exception"},{"l":"dyc.gui"}];updateSearchResults();
